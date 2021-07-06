e87b9dd1f41bfe4608413b46ecaee9bdd7a2e3ca
Kchugames <85512384+Kchugames@users.noreply.github.com>
2021-07-07 00:51:46 +0100

    Add files via upload

---
+++ midcourseproject/MCP1.js
@@ -0,0 +1,32 @@
+
+// function funct1 ()=>
+
+// // make funct that getselementbyid 
+// on click thing in html button tag: when click button JS gets called then happens
+
+function clickAlert() {
+    alert("Were you spooked?");
+    showAlert();
+    }
+
+
+function showAlert() {
+    var myText = "Were you surprised?";
+    prompt (myText);
+    
+}
+
+function picture(){ 
+    var pic = "https://img.icons8.com/ios/50/000000/ghost--v1.png"; alt="cute white ghost button";
+    document.getElementById('blackgh').src = pic;
+    document.getElementById('blackgh').style.width="400px";
+    document.getElementById('blackgh').style.height="300px";   
+    }
+
+
+
+function colourChange() {
+// document.getElementById('colch') .style.backgroundColor ="deeppink";
+document.getElementById('Image1') .style.display ="none"
+
+}
